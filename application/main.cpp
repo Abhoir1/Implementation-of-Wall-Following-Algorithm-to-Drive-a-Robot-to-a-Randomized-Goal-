578b668ff15d6b26a0401b286619e471e6d6468b
sarinann <sarin.ann10@gmail.com>
2022-11-07 19:50:40 -0500

    comments

--- application/main.cpp
+++ application/main.cpp
@@ -1,11 +1,27 @@
+/**
+ * @file main.cpp
+ * @author Sarin Ann Mathew (sarinann@umd.edu)
+ * @author Aditi Bhoir (abhoir@umd.edu)
+ * @author Aditya Chaugule (aditya97@umd.edu)
+ * @brief Main Function
+ * @version 1.0
+ * @date 2022-11-07 
+ * @copyright Copyright (c) 2022
+ */
 
 #include <iostream>
 #include <string>
-
 #include "simulator/simulator.h"
 #include "robot/robot.h"
 #include "algorithm/algorithm.h"
 
+/**
+ * @brief 
+ * 
+ * @param argc - Number of arguments
+ * @param argv - A pointer representing the value of the argument
+ * @return String to the argument "-left" or "-right"
+ */
 int main(int argc, char *argv[])
 {
     rwa2group8::algorithm obj;
